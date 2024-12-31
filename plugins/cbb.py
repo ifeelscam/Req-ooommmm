1bf303c5eeed3cf49148ef0e4745b6f99574123a
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2024-12-31 19:39:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,7 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
+from config import Config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ᴅᴀᴛᴛᴇʙᴀʏᴏ</a>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : <a href='https://t.me/+S6jna9Xe3-UyODhl'>ᴢɪᴘ ғɪʟᴇ </a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={Config.OWNER_ID}'>ᴅᴀᴛᴛᴇʙᴀʏᴏ</a>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : <a href='https://t.me/+S6jna9Xe3-UyODhl'>ᴢɪᴘ ғɪʟᴇ </a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
