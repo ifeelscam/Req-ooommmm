e81f9c794fd5e03847b10a15cdf38326d2099c35
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2024-12-31 18:42:31 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,8 +1,8 @@
-#(©)Codexbotz
+#(©)Codeflix-Bots
 
 from pyrogram import __version__
 from bot import Bot
-from config import Config 
+from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,12 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Bot Owner : <a href='tg://user?id={Config.OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Base Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : <a href='https://t.me/ps_updates'>𝙏𝙃𝙀 𝙋𝙎 𝘽𝙊𝙏𝙎</a>\n○ Support Group : <a href='https://t.me/ps_discuss'>𝙋𝙎 - 𝘿𝙄𝙎𝘾𝙐𝙎𝙎𝙄𝙊𝙉 𝙂𝙍𝙊𝙐𝙋</a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ᴅᴀᴛᴛᴇʙᴀʏᴏ</a>\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : <a href='https://t.me/+S6jna9Xe3-UyODhl'>ᴢɪᴘ ғɪʟᴇ </a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("⚡️ ᴄʟᴏsᴇ", callback_data = "close"),
+                    InlineKeyboardButton('🍁 ᴅᴇᴠʟᴏᴘᴇʀ ', url='https://t.me/Outlawbots')
                     ]
                 ]
             )
